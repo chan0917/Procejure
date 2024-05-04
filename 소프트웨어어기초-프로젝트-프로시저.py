3ac34aa4de7da1fc831ddd8e1f012180a7d1d2f2
Nickname814 <brain0620osj@gmail.com>
2024-05-05 03:09:25 +0900

    소프트웨어기초-프로젝트-프로시저

--- 소프트웨어어기초-프로젝트-프로시저.py
+++ 소프트웨어어기초-프로젝트-프로시저.py
@@ -1,36 +1,34 @@
 ##임시
 while True:
     print("-----------------------------------------------------------------")
-    print('1번')
+    print('1.개좌 개설\t2.개좌 해지\t3.입금')
+    bank_work = int(input())
 
-
-
-    #####
-    class BankServer:  # 1번 조건 객체, 1.1 개인고객이 본인 계좌의 개설에 필요한 정보는 '고객이름, 비밀번호, 최초 입금액' 이다. 
-        def __init__(self, name=None, password=None, price=None, age=None):
-            if (name is not None and password is not None and price is not None) or age is not None:
-                self.server = {
-                    'name': str(name),
-                    'password': password,
-                    'price': int(price),#(수정사항) 최초 입금액 10000원 이상
-                }
-                if age is not None:
-                    self.server['age'] = int(age)
+    all_account = []
+    ## 1번
+    if bank_work == 1:
+        try:
+            customer_name = str(input('이름을 입력하세요.:'))
+            customer_password = (input('비밀번호를 설정하세요.:'))
+            first_cost = int(input('최초 입금액 10,000원 이상을 입금하세요.(,를 제외한 정수만 입력하세오.):'))
+        except:
+            print('형식이 올바르지 않습니다. 다시 시도하세요.')
+            continue
+        if first_cost >= 10000:
+            print("학생이신가요? (y/n)")
+            checking_student = str(input())
+            if checking_student == ('y' or "Y"):      #학생일 경우
+                first_cost += 10000
+                print(first_cost) #임시 출력
+            elif checking_student == ('n' or 'N'):        #아닐 경우 그냥 진행 
+                print('학생X')
+                print(first_cost)
+                continue
             else:
-                print("이름 또는 비밀번호 또는 최초 입금액이 입력 되지 않았습니다.")
-            #(수정사항) 계좌번호 만들기
-
-        def set_value(self, key, value):
-            if key in self.server:
-                self.server[key] = value
-            else:
-                print(f" '{key}' 가 존재 하지 않습니다.")
+                continue
+        else: 
+            continue
             
-    b1 = BankServer('오승재', '1234', 100, 22)
-    print(b1.server)
-
-    #  1.3 학생이 본인 계좌를 개설할 경우, 은행은 신규 가입 선물로 학생의 최초 입금액에 만원(10,000원)을 추가하여 개설한다.
-    if 8 < b1.server.get('age') < 20:
-        b1.server['price'] += 10000
-    print(b1.server)
-
+        
+    else:
+        pass
