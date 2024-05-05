79db11d679ad24607000e7b8671d0ff0be0089ee
Nickname814 <brain0620osj@gmail.com>
2024-05-05 21:33:31 +0900

    소프트웨어 기초 1-1 완성

--- 소프트웨어어기초-프로젝트-프로시저.py
+++ 소프트웨어어기초-프로젝트-프로시저.py
@@ -4,8 +4,9 @@ while True:
     print('1.개좌 개설\t2.개좌 해지\t3.입금')
     bank_work = int(input())
 
-    all_account = []
-    ## 1번
+    all_account = {}
+    ## 1눌렀을 때
+    ### 1-1
     if bank_work == 1:
         try:
             customer_name = str(input('이름을 입력하세요.:'))
@@ -20,15 +21,25 @@ while True:
             if checking_student == ('y' or "Y"):      #학생일 경우
                 first_cost += 10000
                 print(first_cost) #임시 출력
+                all_account['계좌번호'] = [customer_name,customer_password, first_cost]
+                print(all_account)      ####---------------------(확인용 출력)
             elif checking_student == ('n' or 'N'):        #아닐 경우 그냥 진행 
                 print('학생X')
                 print(first_cost)
+                all_account['계좌번호'] = [customer_name,customer_password, first_cost]
+                print(all_account)      ####---------------------(확인용 출력)
                 continue
             else:
                 continue
         else: 
+            print("최초 입금액 10,000원 이상이 필요합니다. 다시 시도하세요.")
             continue
             
-        
+    #2눌렀을 때
+    ## 1-2 
+    elif bank_work == 2:
+        if 
+            print('똥')
     else:
         pass
+
